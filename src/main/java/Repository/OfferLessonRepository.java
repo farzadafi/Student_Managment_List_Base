fa73444f382ce67fa685828c352d29f27e1828f9
farzad <farzadafi40@gmail.com>
2022-01-22 22:00:05 +0330

    add constructor for createTable in OfferLessonRepository

--- src/main/java/Repository/OfferLessonRepository.java
+++ src/main/java/Repository/OfferLessonRepository.java
@@ -1,4 +1,39 @@
 package Repository;
 
-public class OfferLessonRepository {
+import Entity.OfferLesson;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class OfferLessonRepository implements Repository<OfferLesson> {
+    Connection connection = Singleton.getInstance().getConnection();
+
+    public OfferLessonRepository() throws SQLException, ClassNotFoundException {
+        String createTable = "CREATE TABLE IF NOT EXISTS OfferLesson(id serial,lessonName varchar(50),unitNumebr integer)";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+        preparedStatement.execute();
+    }
+
+
+    @Override
+    public int add(OfferLesson offerLesson) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public List<OfferLesson> findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int update(OfferLesson offerLesson) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) throws SQLException {
+        return 0;
+    }
 }
