ae945efcfefb0922a7225ed041d6ab5e55bc49af
farzad <farzadafi40@gmail.com>
2022-01-22 19:56:23 +0330

    add implements StudentRepository from Repository

--- src/main/java/Repository/StudentRepository.java
+++ src/main/java/Repository/StudentRepository.java
@@ -1,4 +1,28 @@
 package Repository;
 
-public class StudentRepository {
+import Entity.Student;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class StudentRepository implements Repository<Student> {
+    @Override
+    public int add(Student student) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public List<Student> findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int update(Student student) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) throws SQLException {
+        return 0;
+    }
 }
