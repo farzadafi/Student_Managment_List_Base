12ed1ff4730a72116cbf606960d4501b0a46a484
farzad <farzadafi40@gmail.com>
2022-01-22 21:59:32 +0330

    add constructor for createTable in LessonRepository

--- src/main/java/Repository/LessonRepository.java
+++ src/main/java/Repository/LessonRepository.java
@@ -2,10 +2,29 @@ package Repository;
 
 import Entity.Lesson;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
 public class LessonRepository implements Repository<Lesson> {
+    Connection connection = Singleton.getInstance().getConnection();
+
+    public LessonRepository() throws SQLException, ClassNotFoundException {
+        String createTable = " CREATE TABLE IF NOT EXISTS Lesson(id serial" +
+                                                                "idStudent integer," +
+                                                                "lessonName varchar(50) REFERENCES OfferLesson(lessonName), " +
+                                                                "nationalId varchar(50) REFERENCES Student(nationalId), " +
+                                                                "quarterNumber integer, " +
+                                                                "unitNumber integer REFERENCES OfferLesson(unitNumber), " +
+                                                                "yearl integer, " +
+                                                                "lastProfessorName varchar(50) REFERENCES Professor(lastName), " +
+                                                                "grade integer)";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+        preparedStatement.execute();
+    }
+
+
     @Override
     public int add(Lesson lesson) throws SQLException {
         return 0;
