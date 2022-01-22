bb6a59cbe23f2eaee81693e8db36dde9f5f08932
farzad <farzadafi40@gmail.com>
2022-01-22 19:56:03 +0330

    add implements ProfessorRepository from Repository

--- src/main/java/Repository/ProfessorRepository.java
+++ src/main/java/Repository/ProfessorRepository.java
@@ -1,4 +1,28 @@
 package Repository;
 
-public class ProfessorRepository {
+import Entity.Professor;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProfessorRepository implements Repository<Professor> {
+    @Override
+    public int add(Professor professor) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public List<Professor> findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int update(Professor professor) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) throws SQLException {
+        return 0;
+    }
 }
