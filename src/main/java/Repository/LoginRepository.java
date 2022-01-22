71824cc605b63d711131c7d3db26fcefbbae8c89
farzad <farzadafi40@gmail.com>
2022-01-22 21:48:34 +0330

    fix LoginRepository and implements from Repository

--- src/main/java/Repository/LoginRepository.java
+++ src/main/java/Repository/LoginRepository.java
@@ -1,4 +1,28 @@
 package Repository;
 
-public class LoginRepository {
+import Entity.Login;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LoginRepository implements Repository<Login> {
+    @Override
+    public int add(Login login) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public List<Login> findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int update(Login login) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) throws SQLException {
+        return 0;
+    }
 }
