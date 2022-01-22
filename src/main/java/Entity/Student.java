e2333bfb201458dcf3875d81ee1d3f8dd1053965
farzad <farzadafi40@gmail.com>
2022-01-22 20:26:42 +0330

    fix Student (extends from User)

--- src/main/java/Entity/Student.java
+++ src/main/java/Entity/Student.java
@@ -1,4 +1,61 @@
 package Entity;
 
-public class Student {
+public class Student extends Person {
+    public Student(String firstName, String lastName, String nationalId, String username, String password) {
+        super(firstName, lastName, nationalId, username, password);
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String getFirstName() {
+        return super.getFirstName();
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        super.setFirstName(firstName);
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName();
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        super.setLastName(lastName);
+    }
+
+    @Override
+    public String getNationalId() {
+        return super.getNationalId();
+    }
+
+    @Override
+    public void setNationalId(String nationalId) {
+        super.setNationalId(nationalId);
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public void setUsername(String username) {
+        super.setUsername(username);
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
 }
