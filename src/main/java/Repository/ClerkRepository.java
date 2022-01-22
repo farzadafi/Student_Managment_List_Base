ec9e244531187aa00714939a2c23b0d158623804
farzad <farzadafi40@gmail.com>
2022-01-22 21:59:13 +0330

    add constructor for createTable in ClerkRepository

--- src/main/java/Repository/ClerkRepository.java
+++ src/main/java/Repository/ClerkRepository.java
@@ -2,10 +2,26 @@ package Repository;
 
 import Entity.Clerk;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
 public class ClerkRepository implements Repository<Clerk> {
+    Connection connection = Singleton.getInstance().getConnection();
+
+    public ClerkRepository() throws SQLException, ClassNotFoundException {
+        String createTable = " CREATE TABLE IF NOT EXISTS Clerk(id serial" +
+                                                        "firstName varcahr(50)," +
+                                                        "lastName varchar(50), " +
+                                                        "nationalId varchar(50), " +
+                                                        "username varchar(50), " +
+                                                        "password varchar(50))";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+        preparedStatement.execute();
+    }
+
+
     @Override
     public int add(Clerk clerk) throws SQLException {
         return 0;
