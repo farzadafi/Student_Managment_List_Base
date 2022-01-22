b1bce4fc68225673c90a632d4804fbc396c8b130
farzad <farzadafi40@gmail.com>
2022-01-22 19:55:48 +0330

    add implements LessonRepository from Repository

--- src/main/java/Repository/LessonRepository.java
+++ src/main/java/Repository/LessonRepository.java
@@ -1,4 +1,28 @@
 package Repository;
 
-public class LessonRepository {
+import Entity.Lesson;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LessonRepository implements Repository<Lesson> {
+    @Override
+    public int add(Lesson lesson) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public List<Lesson> findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int update(Lesson lesson) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) throws SQLException {
+        return 0;
+    }
 }
