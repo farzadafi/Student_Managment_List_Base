e836d82561ae66047424811a02213d46cfe18054
farzad <farzadafi40@gmail.com>
2022-01-22 19:55:35 +0330

    add implements ClerkRepository from Repository

--- src/main/java/Repository/ClerkRepository.java
+++ src/main/java/Repository/ClerkRepository.java
@@ -1,4 +1,28 @@
 package Repository;
 
-public class ClerkRepository {
+import Entity.Clerk;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ClerkRepository implements Repository<Clerk> {
+    @Override
+    public int add(Clerk clerk) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public List<Clerk> findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int update(Clerk clerk) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) throws SQLException {
+        return 0;
+    }
 }
