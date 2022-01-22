5f5cafe701b36a5540ee1ae83217830dc3954ab9
farzad <farzadafi40@gmail.com>
2022-01-22 21:59:48 +0330

    add constructor for createTable in LoginRepository

--- src/main/java/Repository/LoginRepository.java
+++ src/main/java/Repository/LoginRepository.java
@@ -2,10 +2,21 @@ package Repository;
 
 import Entity.Login;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
 public class LoginRepository implements Repository<Login> {
+    Connection connection = Singleton.getInstance().getConnection();
+
+    public LoginRepository() throws SQLException, ClassNotFoundException {
+        String createTable = "CREATE TABLE IF NOT EXISTS Login(username varchar(50) UNIQU ,password varchar(50),kind varchar(10))";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+        preparedStatement.execute();
+    }
+
+
     @Override
     public int add(Login login) throws SQLException {
         return 0;
