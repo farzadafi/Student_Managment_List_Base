2a4caca1460b2af1654036c71795c275e9aa862c
farzad <farzadafi40@gmail.com>
2022-01-23 08:42:52 +0330

    add mainMenu to Menu class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class Menu {
+    private Scanner input = new Scanner(System.in);
+    private int command;
+
+
+
+
+    public int mainMenu(){
+        System.out.println("**********WELCOME**********");
+        System.out.println("1-Enter.");
+        System.out.println("2-Exit.");
+        System.out.print("Please enter your command:");
+        command = input.nextInt();
+        input.nextLine();
+        switch(command) {
+            case 1:
+                return 1;
+
+            case 2:
+                return 2;
+
+            default:
+                return 0;
+        }
+    }
+
+
+
+
+
+
+
+
 }
