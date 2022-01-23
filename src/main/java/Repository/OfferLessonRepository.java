714ba71959e80a5e90c7b1df264cbf27ff12a3d8
farzad <farzadafi40@gmail.com>
2022-01-23 21:18:17 +0330

    fix findAll in OfferLessonRepository

--- src/main/java/Repository/OfferLessonRepository.java
+++ src/main/java/Repository/OfferLessonRepository.java
@@ -1,17 +1,21 @@
 package Repository;
 
+import Entity.Login;
 import Entity.OfferLesson;
+import Entity.TypeUser;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OfferLessonRepository implements Repository<OfferLesson> {
     Connection connection = Singleton.getInstance().getConnection();
 
     public OfferLessonRepository() throws SQLException, ClassNotFoundException {
-        String createTable = "CREATE TABLE IF NOT EXISTS OfferLesson(id serial,lessonName varchar(50),unitNumebr integer)";
+        String createTable = "CREATE TABLE IF NOT EXISTS OfferLesson(id serial,lessonName varchar(50),unitNumber integer)";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(createTable);
             preparedStatement.execute();
@@ -23,7 +27,7 @@ public class OfferLessonRepository implements Repository<OfferLesson> {
 
     @Override
     public int add(OfferLesson offerLesson) throws SQLException {
-        String add = " INSERT INTO OfferLesson(lessonName,unitNumber)";
+        String add = " INSERT INTO OfferLesson(lessonName,unitNumber) VALUES (? ,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(add);
         preparedStatement.setString(1,offerLesson.getLessonName());
         preparedStatement.setInt(2,offerLesson.getUnitNumber());
@@ -37,7 +41,28 @@ public class OfferLessonRepository implements Repository<OfferLesson> {
 
     @Override
     public List<OfferLesson> findAll() throws SQLException {
-        return null;
+        String find = "SELECT * FROM OfferLesson";
+        PreparedStatement preparedStatement = connection.prepareStatement(find);
+        List<OfferLesson> offerLessonList = new ArrayList<>();
+        ResultSet resultSet;
+        try{
+            resultSet = preparedStatement.executeQuery();
+        }catch (SQLException sql){
+            System.out.println(sql.getMessage());
+            return null;
+        }
+        if(resultSet.isBeforeFirst()){
+            while(resultSet.next()) {
+                OfferLesson offerLesson = new OfferLesson();
+                offerLesson.setId(resultSet.getInt("id"));
+                offerLesson.setLessonName(resultSet.getString("lessonName"));
+                offerLesson.setUnitNumber(resultSet.getInt("unitNumber"));
+                offerLessonList.add(offerLesson);
+            }
+            return offerLessonList;
+        }
+        else
+            return null;
     }
 
     @Override
