a22afffec068cbf44343786adc9fa1e41e08369a
farzad <farzadafi40@gmail.com>
2022-01-23 09:17:50 +0330

    add while to main for check input user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,23 @@
+import java.sql.SQLException;
+
 public class App {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Menu menu = new Menu();
+        while (true) {
+            switch (menu.mainMenu()) {
+                case 0:
+                    System.out.println("You enter a wrong number!");
+                    break;
+
+                case 1:
+                    menu.enterMenu();
+                    break;
+
+                case 2:
+                    System.out.println("Have a nice day!");
+                    System.exit(0);
+
+            }
+        }
+    }
 }
