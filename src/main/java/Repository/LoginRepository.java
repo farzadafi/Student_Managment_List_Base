bbc8eec8122aa0bae677a87c451e1a6ab6380a76
farzad <farzadafi40@gmail.com>
2022-01-23 14:50:54 +0330

    fix delete method in LoginRepository

--- src/main/java/Repository/LoginRepository.java
+++ src/main/java/Repository/LoginRepository.java
@@ -72,6 +72,14 @@ public class LoginRepository implements Repository<Login> {
 
     @Override
     public int delete(String username) throws SQLException {
+        String delete = " DELETE FROM Login WHERE username = ? ";
+        PreparedStatement preparedStatement = connection.prepareStatement(delete);
+        preparedStatement.setString(1,username);
+        try {
+            return preparedStatement.executeUpdate();
+        }catch (SQLException sql){
+            System.out.println(sql.getMessage());
+        }
         return 0;
     }
 
