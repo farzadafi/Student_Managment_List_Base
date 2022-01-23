ebada780d882af42346d8a8a2c021f6bfd826d69
farzad <farzadafi40@gmail.com>
2022-01-23 09:48:13 +0330

    fix findAll method for return a list

--- src/main/java/Service/LoginService.java
+++ src/main/java/Service/LoginService.java
@@ -1,4 +1,16 @@
 package Service;
+import Entity.Login;
+import Repository.LoginRepository;
+import java.sql.SQLException;
+import java.util.List;
 
 public class LoginService {
+    private LoginRepository loginRepository = new LoginRepository();
+
+    public LoginService() throws SQLException, ClassNotFoundException {
+    }
+
+    public List<Login> findAll() throws SQLException {
+        return loginRepository.findAll();
+    }
 }
