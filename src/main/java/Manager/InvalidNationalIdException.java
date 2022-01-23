7eab5233ec72273733544918bd8d75c553858ec5
farzad <farzadafi40@gmail.com>
2022-01-23 15:25:05 +0330

    add method for nationalId check to InvalidNationalIdException

--- src/main/java/Manager/InvalidNationalIdException.java
+++ src/main/java/Manager/InvalidNationalIdException.java
@@ -11,4 +11,15 @@ public class InvalidNationalIdException extends RuntimeException {
     public InvalidNationalIdException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public void nationalIdChecker(String nationalId){
+        if(nationalId.length() > 10 )
+            throw new InvalidNationalIdException();
+        if(nationalId.equals(""))
+            throw new InvalidNationalIdException();
+        for (Character ch:nationalId.toCharArray()) {
+            if(!Character.isDigit(ch))
+                throw new InvalidNationalIdException();
+        }
+    }
 }
