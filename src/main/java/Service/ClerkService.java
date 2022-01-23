fc7c382c9a30539c9a2de051c417e43030fa83f2
farzad <farzadafi40@gmail.com>
2022-01-23 20:25:47 +0330

    add updateClerk in ClerkService

--- src/main/java/Service/ClerkService.java
+++ src/main/java/Service/ClerkService.java
@@ -1,9 +1,11 @@
 package Service;
 
 import Entity.Clerk;
+import Entity.Student;
 import Repository.ClerkRepository;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class ClerkService {
     ClerkRepository clerkRepository = new ClerkRepository();
@@ -17,5 +19,13 @@ public class ClerkService {
 
     public int delete(String username) throws SQLException {
         return clerkRepository.delete(username);
+    }
+
+    public List<Clerk> findAll() throws SQLException {
+        return clerkRepository.findAll();
+    }
+
+    public int updateClerk(Clerk clerk) throws SQLException {
+        return clerkRepository.update(clerk);
     }
 }
