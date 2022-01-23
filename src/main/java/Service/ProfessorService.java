90e93810f3660f6dbb627db98c7d158790811fcb
farzad <farzadafi40@gmail.com>
2022-01-23 15:25:41 +0330

    add addProfessor to ProfessorService

--- src/main/java/Service/ProfessorService.java
+++ src/main/java/Service/ProfessorService.java
@@ -1,4 +1,19 @@
 package Service;
 
+import Entity.Login;
+import Entity.Professor;
+import Entity.Student;
+import Repository.ProfessorRepository;
+
+import java.sql.SQLException;
+
 public class ProfessorService {
+    private ProfessorRepository professorRepository = new ProfessorRepository();
+
+    public ProfessorService() throws SQLException, ClassNotFoundException {
+    }
+
+    public void addProfessor(Professor professor) throws SQLException {
+        professorRepository.add(professor);
+    }
 }
