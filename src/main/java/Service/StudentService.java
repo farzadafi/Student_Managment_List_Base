1b17a16fdc628f093acd063121bd64991e82d0d8
farzad <farzadafi40@gmail.com>
2022-01-23 11:09:32 +0330

    add addStudent method i StudentManager

--- src/main/java/Service/StudentService.java
+++ src/main/java/Service/StudentService.java
@@ -1,4 +1,17 @@
 package Service;
 
+import Entity.Student;
+import Repository.StudentRepository;
+
+import java.sql.SQLException;
+
 public class StudentService {
+    private StudentRepository studentRepository = new StudentRepository();
+
+    public StudentService() throws SQLException, ClassNotFoundException {
+    }
+
+    public void addStudnet(Student student) throws SQLException {
+        studentRepository.add(student);
+    }
 }
