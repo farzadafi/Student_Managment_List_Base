c3ff690492ed3e3fcf258cc10d5d2011fcf7a190
farzad <farzadafi40@gmail.com>
2022-01-25 14:48:00 +0330

    add setFirstName and setLastName to Utility

--- src/main/java/Manager/Utility.java
+++ src/main/java/Manager/Utility.java
@@ -5,22 +5,50 @@ import java.util.Scanner;
 public class Utility {
     private Scanner input = new Scanner(System.in);
     private InvalidPassword invalidPassword = new InvalidPassword();
-    private String password;
+    private String password,firstName;
+    private InvalidName invalidName = new InvalidName();
 
     public String setPassword(){
         while(true) {
             System.out.print("Enter your password:");
-            boolean isFalse = true;
             try {
                 password = input.nextLine();
                 invalidPassword.passwordCheck(password);
-                isFalse = false;
+                break;
             } catch (InvalidPassword except) {
                 System.out.println(except.getMessage());
             }
-            if(!isFalse)
-                break;
         }
         return password;
     }
+
+    public String setFirstName(){
+        while(true){
+            System.out.print("Enter first name(just alpha):");
+            try {
+                firstName = input.nextLine();
+                invalidName.checkName(firstName);
+                break;
+            }catch (InvalidName except){
+                System.out.println(except.getMessage());
+            }
+        }
+        return firstName;
+    }
+
+    public String setLastName(){
+        while(true){
+            System.out.print("Enter last name(just alpha):");
+            try {
+                firstName = input.nextLine();
+                invalidName.checkName(firstName);
+                break;
+            }catch (InvalidName except){
+                System.out.println(except.getMessage());
+            }
+        }
+        return firstName;
+    }
+
+    public
 }
