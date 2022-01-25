a2a86f330c731917e995ec99dd666e7b18321322
farzad <farzadafi40@gmail.com>
2022-01-25 14:58:14 +0330

    add setUsername in Utility

--- src/main/java/Manager/Utility.java
+++ src/main/java/Manager/Utility.java
@@ -1,12 +1,22 @@
 package Manager;
 
+import Entity.Login;
+import Service.LoginService;
+
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Utility {
     private Scanner input = new Scanner(System.in);
     private InvalidPassword invalidPassword = new InvalidPassword();
-    private String password,firstName;
+    private String password,firstName,username;
     private InvalidName invalidName = new InvalidName();
+    private InvalidUsername invalidUsername = new InvalidUsername();
+    private LoginService loginService = new LoginService();
+
+    public Utility() throws SQLException, ClassNotFoundException {
+    }
 
     public String setPassword(){
         while(true) {
@@ -50,5 +60,36 @@ public class Utility {
         return firstName;
     }
 
-    public
+    public String setUsername() throws SQLException {
+        while(true) {
+            while(true) {
+                System.out.print("Enter username(start with alpha):");
+                try {
+                    username = input.nextLine();
+                    invalidUsername.checkUsername(username);
+                    break;
+                } catch (InvalidUsername except) {
+                    System.out.println(except.getMessage());
+                }
+            }
+            List<Login> loginList = loginService.findAll();
+            if(loginList == null)
+                break;
+            int i = 0;
+            for (Login login:loginList)
+            {
+                Login login1 = new Login();
+                login1 = login;
+                if(login.getUsername().equals(username)){
+                    i = 1;
+                    break;
+                }
+            }
+            if(i == 1)
+                System.out.println("This username is defined before!");
+            else
+                break;
+        }
+        return username;
+    }
 }
